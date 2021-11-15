1fa33124085d35201f4b14143dc5f578b2daffc4
Sleekgansta <mobinr901@gmail.com>
2021-11-15 19:45:37 +0800

    Fixed Reminders

--- Reminder_Add.php
+++ Reminder_Add.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 $servername='localhost';
 $username='root';
@@ -22,29 +21,3 @@ if(isset($_POST['submit']))
      }
      mysqli_close($conn);
 }
-=======
-<?php
-$servername='localhost';
-$username='root';
-$password='';
-$dbname = "MSP";
-$conn=mysqli_connect($servername,$username,$password,"$dbname");
-
-if(isset($_POST['submit']))
-{    
-     $title = $_POST['ReminderName'];
-     $description = $_POST['Description'];
-	 $dateTime = $_POST['DateTime'];
-	 
-     $sql = "INSERT INTO Reminder (Title,Description,Date_Time)
-			 VALUES ('$title','$description','$dateTime')";
-	 
-     if (mysqli_query($conn, $sql)) {
-        echo "New record has been added successfully !";
-     } else {
-        echo "Error: " . $sql . ":-" . mysqli_error($conn);
-     }
-     mysqli_close($conn);
-}
->>>>>>> 742acbcce634474c8fa5bd5a31d325f2725de98c
-?>
--- Reminder_Delete.php
+++ Reminder_Delete.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 $servername='localhost';
 $username='root';
@@ -19,26 +18,3 @@ if(isset($_POST['submit']))
      }
      mysqli_close($conn);
 }
-=======
-<?php
-$servername='localhost';
-$username='root';
-$password='';
-$dbname = "MSP";
-$conn=mysqli_connect($servername,$username,$password,"$dbname");
-
-if(isset($_POST['submit']))
-{    
-     $title = $_POST['ReminderName'];
-	 
-     $sql = "DELETE FROM Reminder WHERE Title='$title'";
-	 
-     if (mysqli_query($conn, $sql)) {
-        echo "Reminder deleted successfully !";
-     } else {
-        echo "Error: " . $sql . ":-" . mysqli_error($conn);
-     }
-     mysqli_close($conn);
-}
->>>>>>> 742acbcce634474c8fa5bd5a31d325f2725de98c
-?>
